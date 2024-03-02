e630271ab7702dcfde15eace5fad8950480e4afa
Brooke Hinckley <10926137@uvu.edu>
2024-03-01 19:33:53 -0700

    Added joystick controls
    touch wasn't working

---
+++ PlayerJoystickControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJoystickControl : MonoBehaviour
+{
+    public float moveSpeed;        // Player movement speed
+    
+    private CharacterController controller;  // The character controller component
+    private Vector3 velocity;               // Current velocity of the character
+
+    public Joystick joystick;
+    
+    // Awake is called when the script instance is initialized
+    private void Awake()
+    {
+        // Get the CharacterController component attached to this GameObject
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // wasd movement
+
+        var moveH = joystick.Horizontal;
+        var moveDirection =  new Vector3(moveH, 0f, 0f) * moveSpeed;
+
+        //if shift button is pressed, run at twice the speed move speed is set to
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveDirection = new Vector3(moveH, 0f, 0f) * (moveSpeed * 2);
+        }
+
+        // Apply movement and gravity do i need this?
+        var move = moveDirection + velocity;
+        controller.Move(move * Time.deltaTime);
+        
+    }
+
+}
--- PlayerTouchMovement.cs
+++ PlayerTouchMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerTouchMovement : MonoBehaviour
 {
+    public float minX, maxX, minZ, maxZ;
+    
     void Update()
     {
         // Check for touch input
@@ -11,8 +13,16 @@ public class PlayerTouchMovement : MonoBehaviour
             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position); //convert touch position to world space
             touchPosition.y = 0; //should constrain the player to the xz plane
 
+            // Constrain the player to the specified boundaries
+            float x = Mathf.Clamp(transform.position.x, minX, maxX);
+            float z = Mathf.Clamp(transform.position.z, minZ, maxZ);
+            transform.position = new Vector3(x, 0, z);
+            
+            
+            
             // Move based on touch input
-            transform.position = touchPosition;
+            transform.position = touchPosition;            
+            
         }
     }
 }
